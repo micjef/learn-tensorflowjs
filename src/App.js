5bce3ea68087d3d2f95af671e03b667ac4ed9c77
micjef <michaeljeffrysetiawan@gmail.com>
2021-08-31 14:35:32 +0700

    create object detection

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react'
+import React from "react"
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+import ObjectDetection from "./ObjectDetection"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/about">
+            <h1>About</h1>
+          </Route>
+          <Router path="/objectDetection">
+            <ObjectDetection />
+          </Router>
+          <Route path="/">
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/about">About</Link>
+                </li>
+                <li>
+                  <Link to="/objectDetection">Object Detections</Link>
+                </li>
+              </ul>
+            </nav>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
